bb2b5b917bc191ff7a9e6a974467dea45a63fba3
invitado <invitado@s3pc16.eisc.univalle.edu.co>
2018-04-07 11:02:40 -0500

    HAProxy con Stats

--- vagrant/0haproxy/script_hap.sh
+++ vagrant/0haproxy/script_hap.sh
@@ -3,4 +3,4 @@ OUT=/tmp/log.txt
 echo "*** apt-get update"
 sudo apt-get update > ${OUT}
 echo "*** apt-get install HAProxy"
-sudo DEBIAN_FRONTEND=noninteractive  apt-get -y  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install --no-install-recommends haproxy 
+sudo DEBIAN_FRONTEND=noninteractive  apt-get -y  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install --no-install-recommends haproxy >>  ${OUT}
