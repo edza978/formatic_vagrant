f14fdb176c0cfe00aee2539295721d979b584f90
invitado <invitado@s3pc16.eisc.univalle.edu.co>
2018-04-07 09:59:39 -0500

    Avance HAProxy

---
+++ vagrant/0haproxy/script_hap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+OUT=/tmp/log.txt
+echo "*** apt-get update"
+sudo apt-get update > ${OUT}
+echo "*** apt-get install HAProxy"
+sudo DEBIAN_FRONTEND=noninteractive  apt-get -y  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install --no-install-recommends haproxy 
